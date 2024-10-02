638261eff3141228fc80dac5dd1125241aea8522
jiristrouhal <MznStrouhal@gmail.com>
2024-10-02 19:34:28 +0200

    Handle changing size of a dictionary

--- te_tree/tkgui/caseview.py
+++ te_tree/tkgui/caseview.py
@@ -29,64 +29,65 @@ class Case_View_Tk(Case_View):
         style = ttk.Style(window)
         style.configure("Treeview", indent=10)
 
-        self.__tree: ttk.Treeview = ttk.Treeview(window)
-        self.__icons = {label: self.__get_icon(path) for label, path in icons.items()}
-        for label, icon in self.__icons.items():
+        self._tree: ttk.Treeview = ttk.Treeview(window)
+        self._icons = {label: self._get_icon(path) for label, path in icons.items()}
+        collected_icons = self._icons.copy()
+        for label, icon in collected_icons.items():
             if icon is None:
-                self.__icons.pop(label)
-        yscrollbar = ttk.Scrollbar(window, orient="vertical", command=self.__tree.yview)
+                self._icons.pop(label)
+        yscrollbar = ttk.Scrollbar(window, orient="vertical", command=self._tree.yview)
         yscrollbar.pack(anchor=tk.W, fill=tk.Y, side=tk.LEFT)
-        self.__tree.configure(yscrollcommand=yscrollbar, height=25)
+        self._tree.configure(yscrollcommand=yscrollbar, height=25)
 
-        self.__id = str(id(self))
-        self.__lang = lang
+        self._id = str(id(self))
+        self._lang = lang
 
-        self.__tree["columns"] = list(attrs_for_display.keys())
-        self.__attrs_for_display = attrs_for_display
-        self.__precision: int = 28
-        self.__trailing_zeros: bool = False
-        self.__use_thousands_separator: bool = False
+        self._tree["columns"] = list(attrs_for_display.keys())
+        self._attrs_for_display = attrs_for_display
+        self._precision: int = 28
+        self._trailing_zeros: bool = False
+        self._use_thousands_separator: bool = False
 
-        root_item.add_action(self.__id, "adopt", self.__new_item_under_root)
-        root_item.add_action(self.__id, "leave", self.__remove_item)
-        root_item.add_action(self.__id, "rename", self.__rename_item)
-        root_item.add_action_on_set(self.__id, self.__set_displayed_values_of_item_attributes)
+        root_item.add_action(self._id, "adopt", self._new_item_under_root)
+        root_item.add_action(self._id, "leave", self._remove_item)
+        root_item.add_action(self._id, "rename", self._rename_item)
+        root_item.add_action_on_set(self._id, self._set_displayed_values_of_item_attributes)
 
-        self.__tree.bind("<<TreeviewSelect>>", self.__handle_selection_change)
-        self.__tree.bind("<Escape>", lambda e: self.__selection_clear())
-        self.__tree.bind("<Down> <Up>", lambda e: self.__reselect_last())
+        self._tree.bind("<<TreeviewSelect>>", self._handle_selection_change)
+        self._tree.bind("<Escape>", lambda e: self._selection_clear())
+        self._tree.bind("<Down> <Up>", lambda e: self._reselect_last())
 
-        self.__item_dict: Dict[str, Item] = {"": root_item}
-        self.__set_up_headings()
-        self.__reversed_sort: bool = False
-        self.__on_selection_change: List[Callable[[], None]] = list()
+        self._item_dict: Dict[str, Item] = {"": root_item}
+        self._set_up_headings()
+        self._reversed_sort: bool = False
+        self._on_selection_change: List[Callable[[], None]] = list()
 
-        self.__last_selection: str = ""
+        self._last_selection: str = ""
 
     @property
     def id(self) -> str:
-        return self.__id
+        return self._id
 
     @property
     def widget(self) -> ttk.Treeview:
-        return self.__tree
+        return self._tree
 
     @property
     def selected_items(self) -> Set[Item]:
-        return {self.__item_dict[item_id] for item_id in self.__tree.selection()}
+        return {self._item_dict[item_id] for item_id in self._tree.selection()}
 
     def bind(self, sequence: str, action: Callable[[tk.Event], None]) -> None:
-        self.__tree.bind(sequence, action)
+        self._tree.bind(sequence, action)
 
     def configure(self, **kwargs) -> None:
         for label, arg in kwargs.items():
             match label:
                 case "precision":
-                    self.__precision = arg
+                    self._precision = arg
                 case "trailing_zeros":
-                    self.__trailing_zeros = arg
+                    self._trailing_zeros = arg
                 case "use_thousands_separator":
-                    self.__use_thousands_separator = arg
+                    self._use_thousands_separator = arg
                 case _:
                     continue
 
@@ -94,167 +95,167 @@ class Case_View_Tk(Case_View):
         self, action: Callable[[Item, tk.Event], None]
     ) -> Callable[[tk.Event], None]:
         def item_action(event: tk.Event) -> None:
-            tree_item_iid = self.__tree.identify_row(event.y)
-            item = self.__item_dict[tree_item_iid]
+            tree_item_iid = self._tree.identify_row(event.y)
+            item = self._item_dict[tree_item_iid]
             return action(item, event)
 
         return item_action
 
     def is_in_view(self, item_id: str) -> bool:
-        return item_id in self.__item_dict
+        return item_id in self._item_dict
 
     def on_selection_change(self, func: Callable[[], None]) -> None:
-        self.__on_selection_change.append(func)
-        selection = self.__tree.selection()
+        self._on_selection_change.append(func)
+        selection = self._tree.selection()
         if selection:
-            self.__last_selection = selection[-1]
+            self._last_selection = selection[-1]
         else:
-            self.__last_selection = ""
+            self._last_selection = ""
 
     def tree_row_values(self, item_id: str) -> Dict[str, Any]:
         vals: [str, Any] = dict()
-        for label, value in zip(self.__tree["columns"], self.__tree.item(item_id)["values"]):
+        for label, value in zip(self._tree["columns"], self._tree.item(item_id)["values"]):
             vals[label] = value
         return vals
 
-    def __collect_and_set_values(self, item: Item) -> List[str]:
+    def _collect_and_set_values(self, item: Item) -> List[str]:
         values: List[str] = list()
-        for label_group in self.__attrs_for_display:
+        for label_group in self._attrs_for_display:
             values.append("")
-            for label in self.__attrs_for_display[label_group]:
+            for label in self._attrs_for_display[label_group]:
                 if item.has_attribute(label):
                     attr = item.attribute(label)
                     print_args: Dict[str, Any] = dict()
                     if attr.type == "real" or attr.type == "quantity":
-                        print_args["precision"] = self.__precision
-                        print_args["trailing_zeros"] = self.__trailing_zeros
+                        print_args["precision"] = self._precision
+                        print_args["trailing_zeros"] = self._trailing_zeros
                     elif attr.type == "money":
-                        print_args["use_thousands_separator"] = self.__use_thousands_separator
+                        print_args["use_thousands_separator"] = self._use_thousands_separator
                     values[-1] = str(item.attribute(label).print(**print_args))
                     break
         return values
 
-    def __get_icon(self, path: str) -> Optional[ImageTk.PhotoImage]:
+    def _get_icon(self, path: str) -> Optional[ImageTk.PhotoImage]:
         if os.path.isfile(path):
             return ImageTk.PhotoImage(Image.open(path))
         else:
             return None
 
-    def __handle_selection_change(self, event: tk.Event) -> None:
-        for func in self.__on_selection_change:
+    def _handle_selection_change(self, event: tk.Event) -> None:
+        for func in self._on_selection_change:
             func()
 
-    def __new_item(self, item: Item) -> None:
-        values = self.__collect_and_set_values(item)
-        item_iid = self.__tree.insert(
+    def _new_item(self, item: Item) -> None:
+        values = self._collect_and_set_values(item)
+        item_iid = self._tree.insert(
             item.parent.id,
             index=tk.END,
             iid=item.id,
             text=item.name,
             values=values,
         )
-        if item.itype in self.__icons:
-            self.__tree.item(item_iid, image=self.__icons[item.itype])
+        if item.itype in self._icons:
+            self._tree.item(item_iid, image=self._icons[item.itype])
 
-        item.add_action(self.__id, "adopt", self.__new_item)
-        item.add_action(self.__id, "leave", self.__remove_item)
-        item.add_action(self.__id, "rename", self.__rename_item)
-        item.add_action_on_set(self.__id, self.__set_displayed_values_of_item_attributes)
-        self.__item_dict[item.id] = item
+        item.add_action(self._id, "adopt", self._new_item)
+        item.add_action(self._id, "leave", self._remove_item)
+        item.add_action(self._id, "rename", self._rename_item)
+        item.add_action_on_set(self._id, self._set_displayed_values_of_item_attributes)
+        self._item_dict[item.id] = item
 
         for child in item.children:
-            self.__new_item(child)
+            self._new_item(child)
 
-    def __new_item_under_root(self, item: Item) -> None:
-        values = self.__collect_and_set_values(item)
-        self.__tree.insert("", index=tk.END, iid=item.id, text=item.name, values=values)
-        item.add_action(self.__id, "adopt", self.__new_item)
-        item.add_action(self.__id, "leave", self.__remove_item)
-        item.add_action(self.__id, "rename", self.__rename_item)
-        item.add_action_on_set(self.__id, self.__set_displayed_values_of_item_attributes)
-        self.__item_dict[item.id] = item
+    def _new_item_under_root(self, item: Item) -> None:
+        values = self._collect_and_set_values(item)
+        self._tree.insert("", index=tk.END, iid=item.id, text=item.name, values=values)
+        item.add_action(self._id, "adopt", self._new_item)
+        item.add_action(self._id, "leave", self._remove_item)
+        item.add_action(self._id, "rename", self._rename_item)
+        item.add_action_on_set(self._id, self._set_displayed_values_of_item_attributes)
+        self._item_dict[item.id] = item
         for child in item.children:
-            self.__new_item(child)
+            self._new_item(child)
 
-    def __pick_attr_label_from_attrs_assigned_to_caseview_column(
+    def _pick_attr_label_from_attrs_assigned_to_caseview_column(
         self, item: Item, column_label: str
     ) -> str:
-        if column_label not in self.__attrs_for_display:
+        if column_label not in self._attrs_for_display:
             raise Case_View_Tk.Undefined_Column_Label(column_label)
         else:
-            for attr in self.__attrs_for_display[column_label]:
+            for attr in self._attrs_for_display[column_label]:
                 if item.has_attribute(attr):
                     return attr
         # item has no attribute corresponding to the caseview column 'column_label'
         return ""
 
-    def __remove_item(self, item: Item) -> None:
+    def _remove_item(self, item: Item) -> None:
         for child in item.children:
-            self.__remove_item(child)
-        self.__tree.delete(item.id)
-        item.remove_action(self.__id, "adopt")
-        item.remove_action(self.__id, "leave")
-        item.remove_action(self.__id, "rename")
-        item.remove_action_on_set(self.__id)
-        self.__item_dict.pop(item.id)
+            self._remove_item(child)
+        self._tree.delete(item.id)
+        item.remove_action(self._id, "adopt")
+        item.remove_action(self._id, "leave")
+        item.remove_action(self._id, "rename")
+        item.remove_action_on_set(self._id)
+        self._item_dict.pop(item.id)
 
-    def __rename_item(self, item: Item) -> None:
-        self.__tree.item(item.id, text=item.name)
+    def _rename_item(self, item: Item) -> None:
+        self._tree.item(item.id, text=item.name)
 
-    def __set_displayed_values_of_item_attributes(self, item: Item) -> None:
-        values = self.__collect_and_set_values(item)
-        self.__tree.item(item.id, values=values)
+    def _set_displayed_values_of_item_attributes(self, item: Item) -> None:
+        values = self._collect_and_set_values(item)
+        self._tree.item(item.id, values=values)
 
-    def __selection_clear(self) -> None:
-        self.__tree.selection_set([])
+    def _selection_clear(self) -> None:
+        self._tree.selection_set([])
 
-    def __reselect_last(self):
-        if self.__tree.selection() and self.__tree.selection() != [""]:
+    def _reselect_last(self):
+        if self._tree.selection() and self._tree.selection() != [""]:
             return
-        elif self.__last_selection == "" and self.__tree.get_children(""):
-            self.__tree.selection_set(self.__tree.get_children("")[0])
-        elif self.__last_selection in self.__item_dict:
-            self.__tree.selection_set(self.__last_selection)
+        elif self._last_selection == "" and self._tree.get_children(""):
+            self._tree.selection_set(self._tree.get_children("")[0])
+        elif self._last_selection in self._item_dict:
+            self._tree.selection_set(self._last_selection)
 
-    def __set_up_headings(self) -> None:
-        heading_labels = list(self.__tree["columns"])
+    def _set_up_headings(self) -> None:
+        heading_labels = list(self._tree["columns"])
         for heading_label in heading_labels:
-            self.__tree.heading(
+            self._tree.heading(
                 heading_label,
-                text=self.__lang("Item_Attributes", heading_label),
-                command=partial(self.__sort_all_by, heading_label),
+                text=self._lang("Item_Attributes", heading_label),
+                command=partial(self._sort_all_by, heading_label),
                 anchor=tk.CENTER,
             )
-            self.__tree.column(heading_label, anchor=tk.E, minwidth=50, width=100)
-        self.__tree.heading(
+            self._tree.column(heading_label, anchor=tk.E, minwidth=50, width=100)
+        self._tree.heading(
             "#0",
-            text=self.__lang("Item_Attributes", "name"),
-            command=partial(self.__sort_all_by, "#0"),
+            text=self._lang("Item_Attributes", "name"),
+            command=partial(self._sort_all_by, "#0"),
             anchor=tk.CENTER,
         )
-        self.__tree.column("#0", minwidth=100, width=300)
+        self._tree.column("#0", minwidth=100, width=300)
 
-    def __sort_all_by(self, column_label: str) -> None:
-        selection = self.__tree.selection()
+    def _sort_all_by(self, column_label: str) -> None:
+        selection = self._tree.selection()
         if not selection:
             parent_iid = ""
         else:
-            parent_iid = self.__tree.parent(selection[-1])
-        self.__sort_by(column_label, parent_iid=parent_iid)
-        self.__reversed_sort = not self.__reversed_sort
+            parent_iid = self._tree.parent(selection[-1])
+        self._sort_by(column_label, parent_iid=parent_iid)
+        self._reversed_sort = not self._reversed_sort
 
-    def __sort_by(self, column_label: str, parent_iid: str) -> None:
+    def _sort_by(self, column_label: str, parent_iid: str) -> None:
         if column_label == "#0":
             child_data = [
-                (c, self.__tree.item(c)["text"]) for c in self.__tree.get_children(parent_iid)
+                (c, self._tree.item(c)["text"]) for c in self._tree.get_children(parent_iid)
             ]
-            child_data.sort(key=lambda x: x[1], reverse=self.__reversed_sort)
+            child_data.sort(key=lambda x: x[1], reverse=self._reversed_sort)
         else:
             child_data: List[str, Any] = list()
             empty_child_data: List[str, Any] = list()
-            for item_id in self.__tree.get_children(parent_iid):
-                item = self.__item_dict[item_id]
-                attr_label = self.__pick_attr_label_from_attrs_assigned_to_caseview_column(
+            for item_id in self._tree.get_children(parent_iid):
+                item = self._item_dict[item_id]
+                attr_label = self._pick_attr_label_from_attrs_assigned_to_caseview_column(
                     item, column_label
                 )
                 if attr_label == "":
@@ -262,11 +263,11 @@ class Case_View_Tk(Case_View):
                 else:
                     child_data.append((item_id, item(attr_label)))
 
-            child_data.sort(key=lambda x: x[1], reverse=self.__reversed_sort)
+            child_data.sort(key=lambda x: x[1], reverse=self._reversed_sort)
             child_data.extend(empty_child_data)
 
         for index, (c, _) in enumerate(child_data):
-            self.__tree.move(c, parent_iid, index)
+            self._tree.move(c, parent_iid, index)
 
     class Undefined_Column_Label(Exception):
         pass
